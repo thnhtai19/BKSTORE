7b5057c416c5412146cff36b8ed3c6b124ffa67c
Thành Tài <115488732+thnhtai19@users.noreply.github.com>
2024-11-29 17:45:42 +0700

    Update SystemService.php

--- app/models/SystemService.php
+++ app/models/SystemService.php
@@ -48,7 +48,7 @@ class SystemService {
     }
     
     public function getContactInfo() {
-        $sql = "SELECT MaThongTin, Loai, ThongTin, HinhAnh FROM thong_tin_lien_he WHERE TrangThai != 'Đang ẩn' ORDER BY MaThongTin DESC";
+        $sql = "SELECT MaThongTin, Loai, ThongTin, HinhAnh FROM THONG_TIN_LIEN_HE WHERE TrangThai != 'Đang ẩn' ORDER BY MaThongTin DESC";
         $stmt = $this->conn->prepare($sql);
         $stmt->execute();
         $result = $stmt->get_result();
@@ -68,7 +68,7 @@ class SystemService {
     }
 
     public function getSocialInfo() {
-        $sql = "SELECT MaMXH, HinhAnh, LienKet FROM mang_xa_hoi WHERE TrangThai != 'Đang ẩn' ORDER BY MaMXH DESC";
+        $sql = "SELECT MaMXH, HinhAnh, LienKet FROM MANG_XA_HOI WHERE TrangThai != 'Đang ẩn' ORDER BY MaMXH DESC";
         $stmt = $this->conn->prepare($sql);
         $stmt->execute();
         $stmt = $stmt->get_result();
@@ -84,7 +84,7 @@ class SystemService {
     }
 
     public function getPartnerInfo() {
-        $sql = "SELECT MaDoiTac, Ten, HinhAnh, LienKet FROM doi_tac WHERE TrangThai != 'Đang ẩn' ORDER BY MaDoiTac DESC";
+        $sql = "SELECT MaDoiTac, Ten, HinhAnh, LienKet FROM DOI_TAC WHERE TrangThai != 'Đang ẩn' ORDER BY MaDoiTac DESC";
         $stmt = $this->conn->prepare($sql);
         $stmt->execute();
         $stmt = $stmt->get_result();
@@ -118,7 +118,7 @@ class SystemService {
     }
 
     public function getNewsList() {
-        $sql = 'SELECT MaTinTuc, TieuDe, ThoiGianTao, NoiDung, TuKhoa, MoTa FROM tin_tuc ORDER BY MaTinTuc DESC';
+        $sql = 'SELECT MaTinTuc, TieuDe, ThoiGianTao, NoiDung, TuKhoa, MoTa FROM TIN_TUC ORDER BY MaTinTuc DESC';
         $stmt = $this->conn->prepare($sql);
         $stmt->execute();
         $stmt = $stmt->get_result();
@@ -219,7 +219,7 @@ class SystemService {
     }
 
     public function getBannerList() {
-        $sql = 'SELECT * FROM banner ORDER BY MaBanner DESC';
+        $sql = 'SELECT * FROM BANNER ORDER BY MaBanner DESC';
         $stmt = $this->conn->prepare($sql);
         $stmt->execute();
         $stmt = $stmt->get_result();
@@ -288,7 +288,7 @@ class SystemService {
     }
 
     public function getContactList() {
-        $sql = 'SELECT * FROM thong_tin_lien_he ORDER BY MaThongTin DESC';
+        $sql = 'SELECT * FROM THONG_TIN_LIEN_HE ORDER BY MaThongTin DESC';
         $stmt = $this->conn->prepare($sql);
         $stmt->execute();
         $stmt = $stmt->get_result();
@@ -355,7 +355,7 @@ class SystemService {
     }
 
     public function getSocialList() {
-        $sql = 'SELECT MaMXH, HinhAnh, LienKet FROM mang_xa_hoi ORDER BY MaMXH DESC';
+        $sql = 'SELECT MaMXH, HinhAnh, LienKet FROM MANG_XA_HOI ORDER BY MaMXH DESC';
         $stmt = $this->conn->prepare($sql);
         $stmt->execute();
         $stmt = $stmt->get_result();
@@ -420,7 +420,7 @@ class SystemService {
     }
 
     public function getPartnerList() {
-        $sql = 'SELECT MaDoiTac, Ten, HinhAnh, LienKet FROM doi_tac ORDER BY MaDoiTac DESC';
+        $sql = 'SELECT MaDoiTac, Ten, HinhAnh, LienKet FROM DOI_TAC ORDER BY MaDoiTac DESC';
         $stmt = $this->conn->prepare($sql);
         $stmt->execute();
         $stmt = $stmt->get_result();
